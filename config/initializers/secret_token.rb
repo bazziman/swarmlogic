b5083aeadb8ebdf18782a95729a75a988e78deeb
bazziman <bazziman@gmx.de>
2012-02-25 17:45:26 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swarmlogic::Application.config.secret_token = '0a83440ad73a2236c45c61070c8a5879c85a84598b47305368bdc54bb641398ee75179e021753418b4faa7852ced2d95f42b912bcab206af269f23e33dfae41e'
